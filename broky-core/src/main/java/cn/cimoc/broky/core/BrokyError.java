061f277856c64363daafe4beea79baba00b5002b
lgz <sagiri.wo@gmail.com>
2022-12-02 17:54:47 +0800

    refactor(brokyerror): 优化常量构造

--- broky-core/src/main/java/cn/cimoc/broky/core/BrokyError.java
+++ broky-core/src/main/java/cn/cimoc/broky/core/BrokyError.java
@@ -8,10 +8,10 @@ import java.io.Serializable;
  */
 public class BrokyError extends BrokyResult implements Serializable {
 
-    public static final BrokyError SUCCESS = new BrokyError(200, "成功", null);
-    public static final BrokyError PARAM_ERROR = new BrokyError(4001, "参数异常，请检查格式是否正确", null);
-    public static final BrokyError PARAM_VALID_ERROR = new BrokyError(4002, "参数校验不通过，请检查格式是否正确", null);
-    public static final BrokyError UNKNOWN_ERROR = new BrokyError(500, "服务端发生未知错误，请联系开发人员", null);
+    public static final BrokyError SUCCESS = new BrokyError(200, "成功");
+    public static final BrokyError PARAM_ERROR = new BrokyError(4001, "参数异常，请检查格式是否正确");
+    public static final BrokyError PARAM_VALID_ERROR = new BrokyError(4002, "参数校验不通过，请检查格式是否正确");
+    public static final BrokyError UNKNOWN_ERROR = new BrokyError(500, "服务端发生未知错误，请联系开发人员");
 
     public BrokyError(Integer errCode, String errMsg) {
         this(errCode, errMsg, null);
